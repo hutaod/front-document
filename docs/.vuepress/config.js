0a3c60181aa80582a6dfe2fd6beb39f6ee81b8b4
xingyang@123 <1131589588@qq.com>
2019-09-03 19:48:25 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  base: '/frontdocs/',
+  dest: 'public',
+  title: '时霜·前端知识库',
+  description: '前端,前端工程师,前端自学,前端进阶,前端发展',
+  themeConfig: {
+    nav: [
+      { text: '前端', link: '/front/' },
+      { text: 'Nodejs', link: '/nodejs/' },
+      { text: '扩展', link: '/extends/' },
+      { text: 'GitHub', link: 'https://github.com/ht1131589588/web-library' }
+    ]
+  }
+}
---
+++ docs/front/README.md
@@ -0,0 +1,13 @@
+---
+sidebar: auto
+---
+# 前端
+这里记录前端基础知识点和一些框架的使用和分析，分享前端进阶路上遇到的一些难点
+
+## JS
+
+## React
+React
+
+## Webpack
+Webpack
